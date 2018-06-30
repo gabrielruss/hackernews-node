f799947dc2dca6e6139089786b693ba536002878
Gabriel <gabrielruss88@gmail.com>
2018-06-30 17:03:04 -0400

    lots of config

--- src/index.js
+++ src/index.js
@@ -1,52 +1,56 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { Prisma } = require("prisma-binding");
 
 // https://www.howtographql.com/graphql-js/1-getting-started/
-
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
-
-let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (root, { id }) => links.find(link => link.id === id)
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info);
+    }
+    // link: (root, { id }) => links.find(link => link.id === id)
   },
 
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        url: args.url,
-        description: args.description
-      };
-      links.push(link);
-      return link;
-    },
-    updateLink: (root, { id, url, description }) => {
-      let link = Object.assign({}, links.find(link => link.id === id), {
-        url,
-        description
-      });
-      links = [...links.filter(link => link.id !== id), link];
-      return link;
-    },
-    deleteLink: (root, { id }) => {
-      links = [...links.filter(link => link.id !== id)];
-      return;
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description
+          }
+        },
+        info
+      );
     }
+    // updateLink: (root, { id, url, description }) => {
+    //   let link = Object.assign({}, links.find(link => link.id === id), {
+    //     url,
+    //     description
+    //   });
+    //   links = [...links.filter(link => link.id !== id), link];
+    //   return link;
+    // },
+    // deleteLink: (root, { id }) => {
+    //   links = [...links.filter(link => link.id !== id)];
+    //   return;
+    // }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: "src/generated/prisma.graphql",
+      endpoint: "https://us1.prisma.sh/public-pinkdog-479/hackernews-node/dev",
+      secret: "mysecret123",
+      debug: true
+    })
+  })
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
